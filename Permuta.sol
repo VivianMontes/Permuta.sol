ba51b9670e154f4dd386329584631ffb29dbe1ad
Vivian Montes <50385856+sqFNnAdA5GdKKHS@users.noreply.github.com>
2019-05-14 22:38:10 -0300

    Update Permuta.sol

--- Permuta.sol
+++ Permuta.sol
@@ -2,35 +2,35 @@ pragma.solidity 0.5.8;
 
 contract Permuta{
     
-    struct Imovel1; {
+    struct Imovel1 {
         address endereco1;
         string nomeProprietario1;
-        unit valorVenal1;
-    Imovel2 {
+        uint valorVenal1; }
+        
+    struct  Imovel2 { 
         address endereco2;
         string nomeProprietario2;
-        unit valorVenal2;
-    }  
+        unit valorVenal2;}
+      }   
     
     Imovel1[] public livro1;
-    mapping(address => Imovel1) public livro2;
+    mapping(string => Proprietario1) public livro3;
     
     Imovel2[] public livro1;
-    mapping(address => Imovel2) public livro2;
-    
-    function registraImovel (adress paramEndereco1, string memory paramNomeProprietario1, uint paramValorVenal1) public { 
-    Imovel memory novoImovel1 = Imovel(paramEndereco1, paramNomeProprietario1, paramValorVenal1);
-    
-    function registraImovel1 (adress paramEndereco1, string memory paramNomeProprietario2, uint paramValorVenal1);
+    mapping(string => Proprietario2) public livro3;
     
     
+    function permutaImovel1 (adress paramEndereco1, string memory paramNomeProprietario2, uint paramValorVenal1) public { 
+    permutaImovel2 (adress paramEndereco, paramNomeProprietario1, paramValorVenal1);
     
-    livro1.push(novoImovel1);
+ 
+    livro3[paramNomeProprietario2] = Imovel1; 
+    livro3[paramNomeProprietario1] = Imovel2;
     
-    livro2[paramEndereco] = novoImovel1; 
+    livro1.push(Imovel1);
+    livro1.push(Imovel2);
     
-    function devolveNomeProprietario1 (uintmatricula) public view returns (string memory) {
-        return livro1[matricula].nomeProprietario;
+    
     }
 
    
